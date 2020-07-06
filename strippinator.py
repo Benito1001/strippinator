39cde73592f331c6b8d043c37051d56e88e25886
Benito1001 <bendid2103@gmail.com>
2020-07-06 21:04:27 +0200

    first commit

---
+++ strippinator.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import time
+import ctypes
+import PIL.Image as image
+
+def get_new_fraction(width, left, right, fraction):
+	new_width = width - (left+right)
+	if fraction <= 0.5:
+		return (width*fraction - left) / new_width
+	else:
+		return 1 - (width*(1-fraction) - right) / new_width
+
+def get_padding(width, fraction):
+	if fraction <= 0.5:
+		return "left", width*(1 - 2*fraction)
+	else:
+		return "right", width*(2*fraction - 1)
+
+def get_new_padding(width, left, right, fraction):
+	new_fraction = get_new_fraction(width, left, right, fraction)
+	return get_padding(width - (left+right), new_fraction)
+
+def stripImg(img, filename, keep_name=False, center=True, center_frac=None):
+	w, h = img.size
+
+	getSides = ctypes.CDLL('./getSides.so')
+	topy, bottomy, leftx, rightx = (ctypes.c_int(0) for i in range(4))
+	getSides.getSides(ctypes.byref(topy), ctypes.byref(bottomy), ctypes.byref(leftx), ctypes.byref(rightx))
+	topy, bottomy, leftx, rightx = topy.value, bottomy.value, leftx.value, rightx.value
+
+	if center_frac is not None:
+		if center:
+			dir, padding = get_new_padding(w, leftx, w - rightx, center_frac)
+			if dir == "left":
+				leftx -= int(padding)
+			else:
+				rightx += int(padding)
+
+		new_img = image.new('RGB', (rightx - leftx, bottomy - topy), (255, 255, 255))
+		new_img.paste(img, (-leftx, -topy))
+	else:
+		new_img = img.crop([0, topy, w, bottomy])
+
+	if keep_name:
+		new_img.save(filename)
+	else:
+		name, extension = filename.split(".")
+		new_img.save(f"{name}-stripped.{extension}")
+
+center_frac = None
+center = True
+keep_name = False
+args = sys.argv[1:]
+for arg in args[:]:
+	if arg.startswith("--"):
+		if "--crop-sides" in arg:
+			if center_frac is None:
+				center_frac = 0.5
+		if "--no-center" in arg:
+			center = False
+			center_frac = 0.5
+		elif "--center-frac" in arg:
+			center_frac = float(arg.split("=")[1])
+		elif "--keep-name" in arg:
+			keep_name = True
+		args.remove(arg)
+
+if len(args) == 0:
+	pass
+elif args[0] == "all":
+	for file in os.listdir():
+		if not os.path.isdir(file):
+			filetype = file.split(".")[-1].upper()
+			if filetype == "PNG" or filetype == "JPG" or filetype == "JPEG":
+				img = image.open(file)
+				stripImg(img, file, keep_name, center, center_frac)
+else:
+	for file in args:
+		img = image.open(file)
+		stripImg(img, file, keep_name, center, center_frac)
+
+
+# ran as: python3 strippinator.py oppg2d2.png --center-frac=0.924
+
+# start:
+#   get sides: 1.277
+#   fix overfill: 2.239
+#   save img: 1.759
+#   sum: 5.275
+
+# get sides with c++:
+#   get sides: 0.572
+#   fix overfill: 2.166
+#   save img: 1.708
+#   sum: 4.446
+
+# dont fix overfill like a dumbo:
+#   get sides: 0.564
+#   fix overfill: 0.665
+#   save img: 1.746
+#   sum: 2.975
+
+# time saved: 2.3 s, nice
